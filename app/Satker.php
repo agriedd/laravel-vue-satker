bcd6d39b96f9da665b22267a3d637e83f8881629
edd <dnr739528@gmail.com>
2020-11-10 09:17:17 +0800

    migration satker

---
+++ app/Satker.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Satker extends Model{
+    protected $table = 'satker_1';
+    protected $primaryKey = 'id_satker';
+    protected $guarded = [];
+}
---
+++ database/migrations/2020_11_10_011017_create_satkers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSatkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('satker_1', function (Blueprint $table) {
+            $table->id('id_satker');
+            $table->timestamps();
+
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('iso');
+            $table->string('status');
+            $table->text('lain_lain')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('satker_1');
+    }
+}

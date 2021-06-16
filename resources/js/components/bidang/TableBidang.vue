972f7d3305d4597a5a617d0799f846fcee3bc9e2
Agri Edd. Oematan <dnr739528@gmail.com>
2021-06-16 14:56:03 +0800

    menghilangkan kolom pimpinan tableBidang.vue

--- resources/js/components/bidang/TableBidang.vue
+++ resources/js/components/bidang/TableBidang.vue
@@ -62,7 +62,6 @@ export default {
                 { text: 'ID', align: 'start', sortable: true, value: 'id_bidang' },
                 { text: 'Nama Bidang', align: 'start', sortable: true, value: 'nama_bidang' },
                 { text: 'Nama Kepala', align: 'start', sortable: true, value: 'nama_kepala_b' },
-                { text: 'Pimpinan', align: 'center', sortable: false, value: 'pimpinan' },
                 { text: 'Pegawai', align: 'center', sortable: false, value: 'petugas_count' },
                 { text: 'Kegiatan', align: 'center', sortable: false, value: 'kegiatan_count' },
                 { text: 'Satker', align: 'start', sortable: true, value: 'id_satker' },
